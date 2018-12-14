6de90a2ac0b18a11b04ed5dc669c5f11489c639f
bribrah <cheesemas46@gmail.com>
2018-12-13 22:49:26 -0800

    added some CS and polish

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,26 +2,46 @@ require "sinatra"
 require "sinatra/reloader"
 require "erb"
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
 guess_output = ""
+@@guesses_left = 5
 
 get '/' do
     guess = params["guess"].to_i
+    backgroundColor = backgroundColor(guess)
     guess_output = guess(guess)
-    erb :index, :locals => {:guess_output =>guess_output}
+    erb :index, :locals => {:guess_output =>guess_output, :backgroundColor => backgroundColor}
+end
+def backgroundColor(num)
+    if num - @@secret_number > 5 or num - @@secret_number < -5
+        "background: red"
+    elsif num != @@secret_number
+        "background: salmon"
+    else
+        "background: green"
+    end
 end
 
 def guess(num)
-    if num - SECRET_NUMBER > 5
-        "This guess is  way too high"
-    elsif num - SECRET_NUMBER > 0
-        "This guess was a little too high"
-    elsif num - SECRET_NUMBER < -5
-        "This guess was way to low"
-    elsif num - SECRET_NUMBER < 0
-        "This guess was a little to low"
-    else
-        "You are correct!\n The Number was #{SECRET_NUMBER}"
+    @@guesses_left -= 1
+    
+    if num == @@secret_number
+        @@guesses_left = 5
+        @@secret_number = rand(100)
+        "You are correct! The Number was #{@@secret_number}"
+    elsif @@guesses_left == 0
+        @@guesses_left = 5
+        @@secret_number = rand(100)
+        "You ran out of guesses!"
+    elsif num - @@secret_number > 5
+        "This guess is  way too high, , #{@@guesses_left} guesses left"
+    elsif num - @@secret_number > 0
+        "This guess was a little too high, #{@@guesses_left} guesses left"
+    elsif num - @@secret_number < -5
+        "This guess was way to low, #{@@guesses_left} guesses left"
+    elsif num - @@secret_number < 0
+        "This guess was a little to low, #{@@guesses_left} guesses left"
+
     end
 end
 
