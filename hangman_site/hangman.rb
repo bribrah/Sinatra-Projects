b44abd662f050542c9b96fe06c9910836f5758b9
bribrah <cheesemas46@gmail.com>
2018-12-16 14:19:57 -0800

    added lots of work on hangman project

--- hangman_site/hangman.rb
+++ hangman_site/hangman.rb
@@ -7,15 +7,23 @@ require "./lib/Game.rb"
 @@guess_display = ""
 game = Game.new
 
+
 get '/' do
     guess = params["guess"]
     if guess == nil
         guess = ""
     end
-    guess_output = game.makeGuess(guess)
+    if params["new_game"] == "submit"
+        game = game.new
+        params["new_game"] = ""
+    end
+    game.makeGuess(guess)
+    guess_output = game.guess_output
     guess_display = game.guess_display
+    past_guesses = game.past_guesses
+    guesses_left = game.guesses_left
     
-    erb :index, :locals => {:guess_output => guess_output, :guess_display => guess_display}
+    erb :index, :locals => {:guess_output => guess_output, :guess_display => guess_display, :past_guesses => past_guesses, :guesses_left => guesses_left}
 end
 
     
--- hangman_site/lib/Game.rb
+++ hangman_site/lib/Game.rb
@@ -1,10 +1,10 @@
 class Game
     dictionary =  File.read "5desk.txt"
     $dictionary_array = dictionary.split(" ")
-    attr_accessor :guess_display
+    attr_accessor :guess_display, :guess_output, :past_guesses, :guesses_left
     def initialize()
         
-        @incorrect_guesses = 0
+        @guesses_left = 10
         @random_word = ""
         while @random_word.length <5 || @random_word.length > 12
             @random_word = $dictionary_array[rand($dictionary_array.length - 1)].downcase
@@ -12,6 +12,7 @@ class Game
         
         @past_guesses = ""
         @guess_display = ""
+        @guess_output = ""
         @guess_display  = @guess_display.rjust(@random_word.length * 2,"_ ")
         @guess_removed = @random_word
         @original_random_word = @random_word
@@ -20,11 +21,11 @@ class Game
     def check_guess(guess)
         if guess.length != 1 || guess.match?(/[a-zA-Z]/) != true || @past_guesses.match?(guess) == true
             if @past_guesses.match?(guess) == true
-                print "You have already guessed that letter! Please choose again:"
+                @guess_output = "You have already guessed that letter! Please choose again"
             elsif guess == "save"
                 save
             else
-                print "Guess must be one letter, please choose again: "
+                @guess_output = "Guess must be one letter, please choose again"
             end
             return false
         else
@@ -49,6 +50,7 @@ class Game
             return
         else
             @past_guesses += guess
+            @past_guesses = @past_guesses.split('').sort!.join
             
             if @random_word.match(guess) != nil
                 while @guess_removed.index(guess) != nil
@@ -57,16 +59,19 @@ class Game
                     @random_word = @random_word.sub(guess,"_")
                 end
             else
-                return "No occurences of #{guess}, sorry!"
-                @incorrect_guesses += 1
-                if @incorrect_guesses == 10
-                    return "Sorry you lose.  The word was #{@original_random_word}"
-                    return
+                @guess_output = "No occurences of #{guess}, sorry!"
+                @guesses_left -= 1
+                if @guesses_left == 0
+                    @guess_output = "Sorry you lose.  The word was #{@original_random_word}"
                 end
             end
             if @guess_removed == ""
-                return "Congratulations you win!"
+                @guess_output = "Congratulations you win!"
             end
         end
     end
+end
+
+def new_game?
+    initialize
 end
